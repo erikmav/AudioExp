194e2b69c5dd9a5ea52bf951d7f163d8ca6804d7
erikma <erikma1@hotmail.com>
2018-05-29 22:06:19 -0700

    Add Setup.cmd with initial MFCC library

---
+++ Setup.cmd
@@ -0,0 +1,23 @@
+@echo off
+rem Setup script for Windows consoles.
+rem Can be run over and over if you need to. If you see it change when
+rem you do a git pull, you should run it again.
+
+where pip3
+if ERRORLEVEL 1 echo ERROR: pip3 not found. Did you install Python 3.6 or higher? && exit /b 1
+
+echo.
+echo ==========================================================================
+echo Ensuring we have needed Python packages:
+echo   python_speech_features - Mel-Frequency Cepstral Coefficients library.
+echo     https://github.com/jameslyons/python_speech_features
+echo ==========================================================================
+echo.
+call pip3 install python_speech_features
+if ERRORLEVEL 1 echo ERROR: pip3 install failed with errorlevel %ERRORLEVEL% && exit /b 1
+
+echo.
+echo ==========================================================================
+echo Complete!
+echo ==========================================================================
+echo.
